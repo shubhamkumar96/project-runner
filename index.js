a777bd87cb542f5c071c0d3a9e6c6afb82c4a08f
shubhamkumar96 <shubhamkumarranu@gmail.com>
2020-06-11 23:20:57 +0530

    Added 'lodash.debounce, chokidar & caporal' Packages

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 #!/usr/bin/env node
 // Added above line to allow 'index.js' file to be treated as an executable.
 
-console.log("Initial Set-Up Done");
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+const prog = require("caporal");
+
+prog
+  .version("0.0.1")
+  .argument("[filename]", "Name of a file to execute")
+  .action((args) => {
+    const { filename } = args;
+    const name = filename || "index.js";
+
+    const start = debounce(() => {
+      console.log("STARTING USERS PROGRAM");
+    }, 400);
+
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", () => start)
+      .on("unlink", () => start);
+  });
+
+prog.parse(process.argv);
